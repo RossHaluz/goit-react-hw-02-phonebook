7b9529923f5d0865e61a996058b391955cf045df
Rostyslav <rossgaluzinskiy@gmail.com>
2023-01-14 11:39:11 +0200

    add filter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Container, ContactsListTitle, PhonebookTitle } from './App.styled';
 import ContactForm from 'components/AddContacts';
 import ContactsList from 'components/ContactsList';
+import FilterContacts from 'components/FilterContacts';
 import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
     contacts: [],
+    filter: '',
   };
 
   getNewContact = ({ name, number, id }) => {
@@ -21,13 +23,29 @@ class App extends Component {
     }));
   };
 
+  onChancheInputFilter = e => {
+    const { value } = e.currentTarget;
+
+    this.setState({ filter: value });
+  };
+
   render() {
+    const { contacts, filter } = this.state;
+    const visibleTodo = contacts.filter(contact =>
+      contact.name.includes(filter)
+    );
+
     return (
       <Container>
         <PhonebookTitle>Phonebook</PhonebookTitle>
         <ContactForm setContact={this.getNewContact} />
         <ContactsListTitle>Contacts</ContactsListTitle>
-        <ContactsList contacts={this.state.contacts} />
+        <h3>Find contacts by name</h3>
+        <FilterContacts
+          inputValue={filter}
+          changeFilterValue={this.onChancheInputFilter}
+        />
+        <ContactsList contacts={visibleTodo} />
       </Container>
     );
   }
