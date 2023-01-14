9e3abc0156eacd165ad761186a2d6a2e84f1acc5
Rostyslav <rossgaluzinskiy@gmail.com>
2023-01-15 00:50:28 +0200

    Update ContactForm.jsx

--- src/components/AddContacts/ContactForm.jsx
+++ src/components/AddContacts/ContactForm.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 import {
   FormContact,
   FormLable,
   FormInput,
   FormButton,
 } from './ContactForm.styled';
+
+let schema = yup.object().shape({
+  name: yup.string().required(),
+  number: yup.number().min(10).max(12).required(),
+});
 
 const ContactForm = ({ setContact }) => {
   const initialValues = {
@@ -19,13 +25,19 @@ const ContactForm = ({ setContact }) => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmitForm}>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={onSubmitForm}
+    >
       <FormContact>
         <FormLable htmlFor="">Name</FormLable>
         <FormInput type="text" name="name" />
+        <ErrorMessage component="div" name="name" />
 
         <FormLable htmlFor="">Number</FormLable>
         <FormInput type="tel" name="number" />
+        <ErrorMessage component="div" name="number" />
 
         <FormButton type="submit">Add contact</FormButton>
       </FormContact>
